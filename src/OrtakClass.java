47edf8a1bc1b21c7e1b7bb2b2d97719292951c09
Hümeyra Tayfun <humeyratayfun@hotmail.com>
2023-12-14 11:40:37 +0300

    1.gün

---
+++ src/OrtakClass.java
@@ -0,0 +1,6 @@
+public class OrtakClass {
+
+    public static void main(String[] args) {
+        System.out.println("buradan bir rüzgar gecti");
+    }
+}
---
+++ src/Tayfun.java
@@ -0,0 +1,15 @@
+public class Tayfun {
+    public static void main(String[] args) {
+        System.out.println("tayfunun yeri");
+
+        /*
+        git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/HumeyraTayfun/GithupTeamWork.git
+git push -u origin main
+         */
+    }
+
+}
